e43554cc8cfd930377813b86d34be98b794c04ae
PeterKapitonov <79373925842@mail.ru>
2018-03-30 13:51:58 +0300

    добавил условие

--- program.py
+++ program.py
@@ -2,3 +2,7 @@ name = input('Как вас зовут?')
 age = int(input('Привет, '+name+"! сколько тебе лет?\n"))
 f=str(age)
 print("Круто, "+name+"мне тоже "+f+"лет!")
+
+lst = [1, 2, 3]
+if lst :
+   print("hello 4")
